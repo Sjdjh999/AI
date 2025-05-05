add3a16e214efda6404c4f800710cfba8698785c
Sjdjh999 <127460675+Sjdjh999@users.noreply.github.com>
2025-05-05 15:58:34 +0900

    Add files via upload

---
+++ 1018_hw3_202310955.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <queue>
+using namespace std;
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+
+int c, r;
+int map[300][300];
+bool visited[300][300];
+int dx[4] = { -1, 1, 0, 0 };
+int dy[4] = { 0, 0, -1, 1 };
+int IceCount() {
+	
+}
+int IceMelt() {
+
+}
+int main() {
+	scanf("%d %d", &c, &r);
+	
+	for (int i = 0; i < c; i++) {
+		for (int j = 0; j < r; j++) {
+			scanf("%d", map[i][j]);
+		}
+	}
+	int year = 0;
+	while (true) {
+		int ice_count = IceCount();
+		if (ice_count >= 2) {
+			printf("%d", year);
+			break;
+		}
+		if (ice_count == 0) {
+			printf("0");
+			break;
+		}
+		IceMelt();
+		year++;
+	}
+	return 0;
+}*/
