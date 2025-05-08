458139f9436fb4354d4b32bef5a083bef617f68d
Sjdjh999 <127460675+Sjdjh999@users.noreply.github.com>
2025-05-08 19:48:50 +0900

    Add files via upload

---
+++ 1018_hw4_202310955.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <queue>
+using namespace std;
+/*
+int T;
+
+
+struct state {
+	int number;
+
+};
+
+
+int bfs(int start, int finish) {
+	
+}
+int main() {
+	scanf("%d", &T);
+	
+	while (T--) {
+		int A, B;
+		scanf("%d %d", &A, &B);
+		bfs(A, B);
+	}
+
+}*/
