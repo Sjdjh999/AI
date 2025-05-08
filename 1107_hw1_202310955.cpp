6bac98f29d128c6e308331da42b428df969398da
Sjdjh999 <127460675+Sjdjh999@users.noreply.github.com>
2025-05-08 19:49:24 +0900

    Add files via upload

---
+++ 1107_hw1_202310955.cpp
@@ -0,0 +1,42 @@
+//1600 ¸»ÀÌ µÇ°íÇÂ ¿ø¼þÀÌ
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define MAX 200
+#define MAX_K 31
+/*
+bool visited[MAX][MAX][MAX_K];
+int board[MAX][MAX];
+int W, H, K;
+
+int dx[] = { -1,-1,0,0 };
+int dy[] = { 0,0,-1,-1 };
+
+int horse_dx[] = { 2, 2, -2, -2, 1, 1, -1, -1 };
+int horse_dy[] = { 1, -1, 1, -1, 2, -2, 2, -2 };
+
+typedef struct {
+	int x, y;
+	int horse;
+	int moves;
+}State;
+
+int monkey_move() {
+
+}
+
+int main() {
+	scanf("%d", &K);
+	scanf("%d %d", &W, &H);
+
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < W; j++) {
+			scanf("%d", board[i][j]);
+		}
+	}
+
+	printf("%d\n", monkey_move());
+	return 0;
+}
+*/
