f7e77be116d2abfebb58b24f86b06768676834ec
Sjdjh999 <127460675+Sjdjh999@users.noreply.github.com>
2025-05-09 23:05:02 +0900

    Add files via upload

---
+++ 1107_hw2_202310955.cpp
@@ -0,0 +1,44 @@
+//11657 타임머신
+/*
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define MAX_N 500
+#define MAX_M 6000
+
+int N, M;
+Edge edges[MAX_M];
+long long dist[MAX_N + 1];
+
+typedef struct {
+	int u, v, w;
+}Edge;
+
+int time_check(int start) {
+
+}
+
+int main() {
+	scanf("%d %d", &N, &M);
+
+	for (int i = 0; i < M; i++) {
+		int u, v, w;
+		scanf("%d %d %d", &u, &v, &w);
+		edges[i].u = u;
+		edges[i].v = v;
+		edges[i].w = w;
+	}
+	
+	int result = time_check(1);
+	
+	if (result == -1) {
+		printf("-1");
+	}
+	else {
+		for (int i = 2; i <= N; i++)
+		{
+		
+		}
+	
+}*/
