576a55f62db99a8bb0f73872ba5ac5f7eff4c2c0
Sjdjh999 <127460675+Sjdjh999@users.noreply.github.com>
2025-05-14 21:06:44 +0900

    Add files via upload

---
+++ 1121_hw1_202310955.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+
+int parent[1001];
+
+typedef struct {
+    int u, v;
+    double cost;
+} Edge;
+
+typedef struct {
+    int x, y;
+} Point;
+
+int find(int x) {
+
+}
+void union_set(int a, int b) {
+
+}
+int compare(const void* a, const void* b) {
+
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    Point points[n + 1];
+    Edge edges[n * (n - 1) / 2];
+    int edge_count = 0;
+
+    for (int i = 0; i <= n; i++) {
+        scanf("%d %d", &points[i].x, &points[i].y);
+        parent[i] = i;
+    }
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        union_set(u, v);
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = i + 1; j <= n; j++) {
+
+        }
+    }
+    qsort(edges, edge_count, sizeof(Edge), compare);
+
+   
+}*/
