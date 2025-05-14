a6ebdbc6f9863edc757bcbae16d9457554d408c3
Sjdjh999 <127460675+Sjdjh999@users.noreply.github.com>
2025-05-14 21:06:59 +0900

    Add files via upload

---
+++ 1121_hw2_202310955.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+
+#define MAX 50
+#define INF 1e9
+
+int parent[251];
+int map[MAX][MAX];
+int dist[MAX][MAX];
+int dx[] = { 0,0,1,-1 };
+int dy[] = { 1,-1,0,0 };
+
+typedef struct {
+	int x, y;
+} Point;
+
+typedef struct{
+	int u, v, cost;
+} Edge;
+
+
+int compare(const void* a, const void *b) {
+
+}
+
+int dfs() {
+
+}
+
+int main() {
+	int n, num_points = 0;
+	scanf("%d", &n);
+
+	Point points[251];
+	int keys[251][2];
+	char temp[MAX];
+
+	for (int i = 0; i < n; i++) {
+		scanf("%s", &temp);
+		for (int j = 0; j < n; j++) {
+			map[i][j] = temp[j];
+			if (temp[j] == 's' || temp[j] == 'k') {
+				keys[num_points][0] = i;
+				keys[num_points][1] = j;
+
+				points[num_points].x = i;
+				points[num_points].y = j;
+				num_points++;
+			}
+		}
+	}
+	Edge egdes[62500];
+	int edge_count = 0;
+	
+}*/
